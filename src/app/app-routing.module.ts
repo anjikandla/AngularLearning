44f1afdc28dd41c04ef03c424b6b00021d3c0b19
unknown <anjikandla@gmail.com>
2022-03-15 08:56:40 +0530

    add color component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ColorPageComponent } from './color-page/color-page.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'app-color-page', component: ColorPageComponent },
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
